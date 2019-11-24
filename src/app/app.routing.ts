fcae7cd1750e74aab34d4554bf8ca6e8ac0ace04
Luiz <luiz.fernando360@gmail.com>
2019-11-24 18:03:08 -0300

    tela de login

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,21 +1,14 @@
-import { ClienteFormComponent } from './cliente/cliente-form/cliente-form.component';
 import { ModuleWithProviders } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { ClienteComponent } from './cliente/cliente.component';
 import { LoginComponent } from './login/login.component';
-import { ClienteDetalheComponent } from './cliente-detalhe/cliente-detalhe.component';
-import { VendedorComponent } from './vendedor/vendedor.component';
+
 
 
 
 const APP_ROUTES: Routes = [
-  {path: '', component: HomeComponent},
-  {path: 'login', component: LoginComponent},
-  {path: 'cliente', component: ClienteComponent},
-  {path: 'cliente/cliente-form', component: ClienteFormComponent},
-  {path: 'cliente/:id', component: ClienteDetalheComponent},
-  {path: 'vendedor', component: VendedorComponent}
+  {path: '', component: LoginComponent},
+  {path: 'login', component: LoginComponent}
 ];
 
 export const routing: ModuleWithProviders = RouterModule.forRoot(APP_ROUTES);
